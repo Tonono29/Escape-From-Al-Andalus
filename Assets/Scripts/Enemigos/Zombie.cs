7701851a40fe9f31e6805d66066c5765a766ec1c
Tonono29 <aherriv@gmail.com>
2022-05-20 18:03:10 +0200

    con instituto

--- Assets/Scripts/Enemigos/Zombie.cs
+++ Assets/Scripts/Enemigos/Zombie.cs
@@ -21,6 +21,10 @@ public class Zombie : MonoBehaviour
         buscando = new Buscar(maquinaEstados, agenteNav, this);
         persiguiendo = new Perseguir(maquinaEstados, agenteNav, this);
         maquinaEstados.Inicializar(patrullando);
+        for (int y=0;y<destinos.Length;y++)
+        {
+            destinos[y].y = transform.position.y;
+        }
     }
     private void Update()
     {
