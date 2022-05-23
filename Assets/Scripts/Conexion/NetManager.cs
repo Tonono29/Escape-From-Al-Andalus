3b092a5cbc83b4c96d0c176cdcd481edc07c225e
joseantorrecillas <joseantonio.garcia.alumnado@iesalandalus.org>
2022-05-23 19:21:37 +0200

    interfaz multi
    interfaz multi

---
+++ Assets/Scripts/Conexion/NetManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetManager : MonoBehaviour
+{
+    public GameObject spaw1;
+    public GameObject spaw2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numJugador = PhotonNetwork.LocalPlayer.ActorNumber;
+
+        if (numJugador == 1)
+        {
+            PhotonNetwork.Instantiate("car", spaw1.transform.position, Quaternion.identity);
+        }
+        else if (numJugador == 2)
+        {
+            PhotonNetwork.Instantiate("car", spaw2.transform.position, Quaternion.identity);
+        }
+    }    
+}
