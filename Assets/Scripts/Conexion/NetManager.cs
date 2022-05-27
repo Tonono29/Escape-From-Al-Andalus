6c350089dce604618e02a7b2fb0cdde1102e5b27
joseantorrecillas <joseantonio.garcia.alumnado@iesalandalus.org>
2022-05-27 18:26:18 +0200

    Multijugador funcional
    Multijugador funcional

--- Assets/Scripts/Conexion/NetManager.cs
+++ Assets/Scripts/Conexion/NetManager.cs
@@ -2,11 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Voice;
+using Photon.Voice.PUN;
+using Photon.Voice.Unity;
 
 public class NetManager : MonoBehaviour
 {
     public GameObject spaw1;
     public GameObject spaw2;
+    private PhotonVoiceNetwork chat;
+    private Recorder rec;
 
     // Start is called before the first frame update
     void Start()
@@ -21,5 +26,35 @@ public class NetManager : MonoBehaviour
         {
             PhotonNetwork.Instantiate("Jugador02", spaw2.transform.position, Quaternion.identity);
         }
-    }    
+        //StartCoroutine("conectarChat");
+    }
+    private IEnumerator conectarChat()
+    {
+        while (chat.ClientState != Photon.Realtime.ClientState.Joined)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+
+        int numJugador = PhotonNetwork.LocalPlayer.ActorNumber;
+
+        switch (numJugador)
+        {
+            case 1:
+                chat.Client.OpChangeGroups(null, new byte[1] { 1 });
+                rec.InterestGroup = 1;
+                break;
+            case 2:
+                chat.Client.OpChangeGroups(null, new byte[1] { 1 });
+                rec.InterestGroup = 1;
+                break;
+            case 3:
+                chat.Client.OpChangeGroups(null, new byte[1] { 2 });
+                rec.InterestGroup = 2;
+                break;
+            case 4:
+                chat.Client.OpChangeGroups(null, new byte[1] { 2 });
+                rec.InterestGroup = 2;
+                break;
+        }
+    }
 }
