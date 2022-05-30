2dd9adfcdf44f4e43528dd0dc1964940ca9adad6
Tonono29 <aherriv@gmail.com>
2022-05-30 16:39:30 +0200

    actualizar

--- Assets/Scripts/Jugador/PlayerController.cs
+++ Assets/Scripts/Jugador/PlayerController.cs
@@ -1,7 +1,9 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] Text textoui;
     [SerializeField] private Camera miCamara;
     CharacterController characterController;
     public float MovementSpeed =1;
@@ -9,14 +11,14 @@ public class PlayerController : MonoBehaviour
     private float velocity = 0;
     RaycastHit miRayito;
     #region Delegados
-    public delegate void Manejadorpuerta(bool estadoPuerta);
+    //public delegate void Manejadorpuerta(bool estadoPuerta);
     public delegate void ManejadorAbrirCerrar();
-    public delegate void Limpiarpuerta();
+    //public delegate void Limpiarpuerta();
     #endregion
     #region Eventos
-    public event Manejadorpuerta OnInteraccionPuerta;
+    //public event Manejadorpuerta OnInteraccionPuerta;
     public event ManejadorAbrirCerrar OnAbrirCerrar;
-    public event Limpiarpuerta OnLimpiar;
+    //public event Limpiarpuerta OnLimpiar;
     #endregion
     public static PlayerController Instancia { get; private set; }
     private void Awake()
@@ -57,11 +59,12 @@ public class PlayerController : MonoBehaviour
         {
             if (miRayito.transform.gameObject.tag == "Puerta")
             {
-                OnInteraccionPuerta?.Invoke(miRayito.transform.gameObject.GetComponent<InteraccionPuerta>().puertaAbierta);
+                //OnInteraccionPuerta?.Invoke(miRayito.transform.gameObject.GetComponent<InteraccionPuerta>().puertaAbierta);
+                miRayito.transform.gameObject.GetComponent<InteraccionPuerta>().MostrarUiPuertas();
             }
             else
             {
-                OnLimpiar?.Invoke();
+                textoui.text = "";
             }
             Debug.DrawRay(miCamara.transform.position, miCamara.transform.forward, Color.yellow);
         }
