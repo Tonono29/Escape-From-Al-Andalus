c6e244c0fbdf5141879af3b5c95ce6be90b05235
Tonono29 <aherriv@gmail.com>
2022-06-07 19:47:17 +0200

    juego final

--- Assets/Scripts/Interaccion/Puerta/PuertaLllave.cs
+++ Assets/Scripts/Interaccion/Puerta/PuertaLllave.cs
@@ -24,7 +24,7 @@ public class PuertaLllave : MonoBehaviour
     }
     public void CerrarPuerta()
     {
-        puerta1.gameObject.transform.RotateAround(objetoPivote.transform.position, Vector3.up * -1, 120);
+        puerta1.gameObject.transform.RotateAround(objetoPivote1.transform.position, Vector3.up * -1, 120);
         puerta.gameObject.transform.RotateAround(objetoPivote.transform.position, Vector3.up * 1, 120);
     }
     public void AbrirCerrar()
